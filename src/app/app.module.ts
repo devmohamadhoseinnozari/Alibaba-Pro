cd122cf1a13e0b2c254f3d7444ebfb6f2fd6f308
mohamadhosein <mohamadhoseinnozaridev@gmail.com>
2023-10-29 12:22:50 +0330

    bild suport__page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { DiscriptionAlibabComponent } from './components/discriptionAlibab/discriptionAlibab.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FooterseviceComponent } from './components/Footersevice/Footersevice.component';
+import { Support_pageComponent } from './page/Support_page/Support_page.component';
+import { Main_suportComponent } from './components/main_suport/main_suport.component';
+import { Main_suport_downComponent } from './components/main_suport_down/main_suport_down.component';
+import { Suport_quickComponent } from './components/suport_quick/suport_quick.component';
+import { Suport_tour_guideComponent } from './components/suport_tour_guide/suport_tour_guide.component';
+import { SafariComponent } from './page/safari/safari.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,6 +33,12 @@ import { FooterseviceComponent } from './components/Footersevice/Footersevice.co
     DiscriptionAlibabComponent,
     FooterComponent,
     FooterseviceComponent,
+    Support_pageComponent,
+    Main_suportComponent,
+    Main_suport_downComponent,
+    Suport_quickComponent,
+    Suport_tour_guideComponent,
+    SafariComponent,
   ],
   imports: [
     BrowserModule,
